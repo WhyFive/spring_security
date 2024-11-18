d4c781efd067ed1c203d609c63d3463faa05860a
WhyFive <jyichyuan2003@gmail.com>
2024-11-18 16:08:35 +0800

    first commit

---
+++ src/main/java/com/example/springSecurity/Controller/RegistrationController.java
@@ -0,0 +1,22 @@
+package com.example.springSecurity.Controller;
+
+import com.example.springSecurity.Entity.MyUser;
+import com.example.springSecurity.Repository.MyUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+    @Autowired
+    private MyUserRepository myUserRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @PostMapping("/register/user")
+    public MyUser createUser(@RequestBody MyUser user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return myUserRepository.save(user);
+    }
+}
---
+++ src/main/java/com/example/springSecurity/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,24 @@
+package com.example.springSecurity;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        boolean isAdmin = authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
+
+        if (isAdmin) {
+            response.sendRedirect("/admin/home");
+        } else {
+            response.sendRedirect("/user/home");
+        }
+    }
+}
